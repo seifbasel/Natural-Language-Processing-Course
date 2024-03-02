df3eb33f60d873daaea1a2ce75809e4d924f5031
seif basel <seifbasel950@gmail.com>
2024-03-02 16:13:55 +0200

    done

--- task1.py
+++ task1.py
@@ -12,12 +12,12 @@ def nat_language_proc(choice, text):
         words = text.split()
         return words
 
-print("enter a number from 1 for words or 2 for sentences  or 3 for split")
-choice=int(input())
-while choice not in [1,2,3]:
-    print("enter a number from 1 for words or 2 for sentences  or 3 for split")
+for i in range(3):
+    print("enter a number from / 1 for words / 2 for sentences / 3 for split")
     choice=int(input())
-
-result = nat_language_proc(choice, """i have traveled to las vegas, new york and egypt last year.
-                   many of my friends also traveled to france and spain.""")
-print(result)
+    while choice not in [1,2,3]:
+        print("enter a number from 1 for words or 2 for sentences  or 3 for split")
+        choice=int(input())
+    result = nat_language_proc(choice, """i have traveled to las vegas, new york and egypt last year.
+                    many of my friends also traveled to france and spain.""")
+    print(result)
